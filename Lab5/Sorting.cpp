b11c3c59bb1596df756368271afdb10b0667d78f
Logan Daniels <daniellg@mail.uc.edu>
2019-06-16 22:27:03 -0400

    Bubble Sort 2

--- Lab5/Sorting.cpp
+++ Lab5/Sorting.cpp
@@ -2,30 +2,33 @@
 using namespace std;
 
 
-const int n = 10;
-
-
 int main () {
 
+const int n = 10;
 int tempLoc;
 const int sizeofArray = n;
-int sortingArray[sizeofArray];
-int length = n;
-int end = n;
+int sortingArray[sizeofArray]; 
+int length = 10;
+int end = 10;
 
 for (int counter = length - 1; counter > 0; counter--)
 {
+
 	for (int i = 0; i < sizeofArray; i++)
 	{
+
 	   sortingArray[i] = rand() % 20;
 
 		if (sortingArray[i] > sortingArray[i + 1])
 		{
+
 			tempLoc = sortingArray[i + 1];
 			sortingArray[i + 1] = sortingArray[1];
 			sortingArray[i] = tempLoc;
 		}
+
 		end--;
+
 	}
 
 	for (int i = 0; i < sizeofArray; i++)
